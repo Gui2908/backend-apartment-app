88535436ae8b2a69a72b51de9dc92367e6575e50
gui2908 <guilherme2908@hotmail.com>
2018-09-25 09:04:02 -0700

    continue writing tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :apartments
   devise :database_authenticatable,
          :registerable,
          :validatable,
